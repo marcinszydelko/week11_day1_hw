98417400adefe3bd78eccb217e0d9ab8fbc8631e
Marcin <marcinszydelko@yahoo.com>
2020-01-06 15:03:10 +0000

    add function/test divide

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -11,4 +11,8 @@ public class Calculator {
     public double multiply(double a, double b){
         return a * b;
     }
+
+    public double divide(double a, double b){
+        return a / b;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -23,6 +23,10 @@ public class CalculatorTest {
         assertEquals(5, calculator.multiply(2.5, 2), 0.01);
     }
 
-    
+    @Test
+    public void canDivide(){
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.divide(15, 3), 0.01);
+    }
 
 }
